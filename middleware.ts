77a8f8e896c585189495f317848822366c609b35
Flashyrs <roshanshuklayt@gmail.com>
2025-11-20 11:18:21 +0530

    added middleware to route to https

---
+++ middleware.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export function middleware(request: NextRequest) {
+  if (request.headers.get("x-forwarded-proto") === "http") {
+    const url = request.nextUrl;
+    url.protocol = "https:";
+    return NextResponse.redirect(url);
+  }
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ['/:path*'],
+};
